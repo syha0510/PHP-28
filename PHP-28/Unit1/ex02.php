17ecdaa7be64b7db8bb56199b1c261ea05eb1302
syha0510 <syha2257@gmail.com>
2021-06-18 19:31:19 +0700

    nopbai php

---
+++ PHP-28/Unit1/ex02.php
@@ -0,0 +1,16 @@
+<?php
+echo "hello world,hello Zent coding school";
+?>
+<?= " <br>hello,PHP Khó vch " ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>is</title>
+</head>
+<body>
+	<h1><?php echo "hello "?></h1>
+  <h2><?= "Trần Sỹ Hà"?></h2>
+</body>
+</html>
---
+++ PHP-28/unit2/index1.php
@@ -0,0 +1,12 @@
+<?php
+  $numb=10;
+  $point=9.5;
+  $name="Zent";
+  var_dump($numb);
+  var_dump($point);
+  var_dump($name);
+
+  echo "<br>Xin chào" .$name;
+  echo "<br>Xin chào $name" ;
+  echo '<br>Xin chào' .$name ;
+?>
